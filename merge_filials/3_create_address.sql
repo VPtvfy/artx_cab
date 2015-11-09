5b5da5cb8caa3efe597f56dfbafda01f3fcb6999
VPtvfy <zeman@rambler.ru>
2015-11-09 19:11:45 +0600

    phone and address not unique constraint disabled

--- merge_filials/3_create_address.sql
+++ merge_filials/3_create_address.sql
@@ -10,10 +10,10 @@ create table artex_all.street (
   key idx_street(street_name)
   ) engine=innodb default charset=utf8;
   
-INSERT INTO artex_all.street(street_id,town_id,street_name)
-        VALUES (0,null,'Без адреса');
+-- INSERT INTO artex_all.street(street_id,town_id,street_name)
+--        VALUES (0,null,'Без адреса');
 
-UPDATE artex_all.street SET street_id=0;
+-- UPDATE artex_all.street SET street_id=0;
 
 ALTER TABLE artex_all.street  
 AUTO_INCREMENT=1;
@@ -24,25 +24,31 @@ alter table artex_all.street
 alter table artex_all.street 
   add constraint fk_street_town foreign key (town_id) references artex_all.town(town_id) on update cascade;
 
+update _firm 
+   set street='' 
+ where CHAR_LENGTH(street)<3;
+
 insert into artex_all.street(town_id,street_name) 
 select distinct t.town_id,f.street 
   from artex_all._firm f
   left join artex_all.town t on t.town_name=f.town
- where street is not null and char_length(street) >3
- order by 1,2;
+ where street is not null 
+   and t.town_id is not null 
+--   and CHAR_LENGTH(street)>3
+ order by 2,1;
  
 update artex_all.street  set street_name=replace(trim(street_name),'  ',' ') where street_name regexp '[[:blank:]]{2,}';
 
 create table artex_all.firm_address(
   address_id int(6) unsigned not null auto_increment,
   firm_id int(6) unsigned not null,
-  street_id int(6)unsigned,
+  street_id int(6) unsigned not null,
   building int(4) unsigned not null,
   bletter char(1) not null,
   office int(4) unsigned,
   oletter char(1) not null,
   description varchar(256),
-  primary key  (address_id));
+  primary key (address_id));
 
 alter table artex_all.firm_address
   add unique index unq_address (firm_id,street_id,building,bletter,office,oletter);
@@ -58,6 +64,7 @@ alter table artex_all.firm_address
 
 alter table artex_all.firm_address
   add constraint fk_firm_address   foreign key (firm_id)   references artex_all.firm(firm_id) on update cascade;
+
 
 update _firm 
    set building=0 
@@ -79,19 +86,18 @@ update _firm
     
 update _firm 
    set oletter=''
- where trim(oletter)='0';   
+ where trim(oletter)='0'; 
  
-
 insert into artex_all.firm_address(firm_id,street_id,building,bletter,office,oletter)
-select distinct a.firm_id,s.street_id,f.building,f.bletter,f.office,f.oletter 
+select a.firm_id,s.street_id,f.building,f.bletter,f.office,f.oletter 
   from artex_all._firm f
  inner join artex_all.firm a on a.firm_name=f.firm_name
   left join artex_all.town t on t.town_name=f.town 
   left join artex_all.street s on f.street=s.street_name and s.town_id=t.town_id
- where s.town_id is not null 
- group by a.firm_id,s.street_id,f.building,f.bletter,ifnull(f.office,a.firm_id),f.oletter 
- order by 2,3,1
-    on duplicate key update office=null;
+ where s.street_id is not null 
+ group by f.firm_name,f.town,f.street,f.building,f.bletter,ifnull(f.office,a.firm_id),f.oletter 
+ order by 2,3,1;
+--    on duplicate key update office=null;
  
 create table artex_all.firm_phone(
   phone_id	int(6) unsigned not null auto_increment,
@@ -112,14 +118,14 @@ alter table artex_all.firm_phone
 add constraint fk_firm_phone_address foreign key (address_id) references artex_all.firm_address(address_id) on update cascade;
 
 insert into artex_all.firm_phone(address_id,phone_type,phone_code,phone_number,phone_description)
-select distinct d.address_id,1,t.code,f.phone,f.div_name 
+select d.address_id,1,t.code,f.phone,f.div_name 
   from artex_all._firm f
   left join artex_all.firm a on a.firm_name=f.firm_name
   left join artex_all.town t on f.town=t.town_name
   left join artex_all.street s on f.street=s.street_name and s.town_id=t.town_id
   left join artex_all.firm_address d on d.firm_id=a.firm_id and d.street_id=s.street_id and d.building=f.building and d.bletter=f.bletter and ifnull(d.office,'')=ifnull(f.office,'') and d.oletter=f.oletter
- where d.address_id is not null and length(phone) in (3,6,10) 
- group by a.firm_id,d.address_id,t.code,f.phone;
+ where d.address_id is not null
+ group by f.firm_name,f.town,f.phone;
  
 create or replace view vgfirm as 
 select f.firm_id,s.town_id,c.item_id,a.address_id,p.phone_id,
